134487bd5f8dc55c95f5332a6b1588556c9958ec
Santiago G <Santiagoefrad@outlook.com>
2020-08-26 17:32:43 -0700

    Did the character

--- Capitalism/Player.cs
+++ Capitalism/Player.cs
@@ -8,10 +8,32 @@ using System.Threading.Tasks;
 
 namespace Capitalism
 {
-    public class Player : Sprite
+    public class Player
     {
-        public Player(Texture2D image, Vector2 position, Color tint) : base(image, position, tint)
+        public Texture2D Image;
+        public Vector2 Position;
+        public Color Tint;
+        public Rectangle Hitbox => new Rectangle((int)Position.X, (int)Position.Y, Image.Width, Image.Height);
+
+        //yet to be decided if I use a set of bills, or I just do the number.
+        public string Token = "";
+
+        public Player(Texture2D image, Vector2 position, Color tint, string token)
         {
+            Position = position;
+            Image = image;
+            Tint = tint;
+            Token = token;
+        }
+
+        public void Update()
+        { 
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Image, Position, Tint);
         }
     }
 }
--- Capitalism/Screens/ChoosingCharacters.cs
+++ Capitalism/Screens/ChoosingCharacters.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace Capitalism.Screens
 {
@@ -10,6 +11,8 @@ namespace Capitalism.Screens
     {
         SpriteFont playerFont;
 
+
+        #region Token Setup
         Texture2D VroomVroom;
         Texture2D AAAANNNNNNNNNDDDDDDDDD_THE_TITANIC_HAS_SUNK_LADIES_AND_GENTLEMEN;
         Texture2D good_day_sir;
@@ -31,20 +34,36 @@ namespace Capitalism.Screens
         Button Plane;
         Button Duck;
 
+        bool[] alreadySelected = new bool[9];
 
+        #endregion
         public int playerCount => SelectingPlayers.playerCount;
         int currentPlayer = 1;
 
+        List<Player> Players = new List<Player>();
+
         // LIST OF PLAYERS  - Car, Ship, Top Hat, Cat, Dog, WheelBarrow, Boot, Plane, Hamburger, duck
 
+        Dictionary<string, (Player player, Button button)> players = new Dictionary<string, (Player, Button)>();
+
         public ChoosingCharacters(string Name) : base(Name)
-        { 
-        
+        {
+
         }
 
         public override void LoadContent(ContentManager Content)
         {
+
+            for (int i = 0; i < alreadySelected.Length; i++)
+            {
+                alreadySelected[i] = true;
+            }
+
             playerFont = Content.Load<SpriteFont>("startingScreenFont");
+
+
+
+            #region tokens 
 
             VroomVroom = Content.Load<Texture2D>("car");
             AAAANNNNNNNNNDDDDDDDDD_THE_TITANIC_HAS_SUNK_LADIES_AND_GENTLEMEN = Content.Load<Texture2D>("boat");
@@ -58,27 +77,99 @@ namespace Capitalism.Screens
 
             Car = new Button(VroomVroom, new Vector2(50, 120), Color.White);
             Boat = new Button(AAAANNNNNNNNNDDDDDDDDD_THE_TITANIC_HAS_SUNK_LADIES_AND_GENTLEMEN, new Vector2(260, 100), Color.White);
-            Hat = new Button(good_day_sir, new Vector2(480, 120), Color.White);
+            Hat = new Button(good_day_sir, new Vector2(480, 130), Color.White);
             Cat = new Button(ew, new Vector2(50, 260), Color.White);
-            Wheelbarrow = new Button(discount_cart, new Vector2(260, 260), Color.White);
+            Wheelbarrow = new Button(discount_cart, new Vector2(260, 320), Color.White);
             Boot = new Button(the_shape_of_italy, new Vector2(50, 470), Color.White);
 
+
             Duck = new Button(weakling, new Vector2(480, 470), Color.White);
+            #endregion
+
+            players.Add("Car", (Players[0], new Button(VroomVroom, new Vector2(50, 120), Color.White)));
+            players.Add("Ship", (Players[1], new Button(AAAANNNNNNNNNDDDDDDDDD_THE_TITANIC_HAS_SUNK_LADIES_AND_GENTLEMEN, new Vector2(260, 100), Color.White)));
+            players.Add("Hat", (Players[2], new Button(VroomVroom, new Vector2(50, 120), Color.White)));
 
         }
+
 
         public override void Update(GameTime gameTime)
         {
             MouseState ms = Mouse.GetState();
-            Car.Update(ms, true);
-            Boat.Update(ms, true);
-            Hat.Update(ms, true);
-            Cat.Update(ms, true);
 
-            Wheelbarrow.Update(ms, true);
-            Boot.Update(ms, true);
+            //foreach (var pair in players)
+            //{
+            //    pair.Value.Update();
+            //}
 
-            Duck.Update(ms, true);
+
+            Car.Update(ms, alreadySelected[0]);
+            Boat.Update(ms, alreadySelected[1]);
+            Hat.Update(ms, alreadySelected[2]);
+            Cat.Update(ms, alreadySelected[3]);
+            Wheelbarrow.Update(ms, alreadySelected[5]);
+            Boot.Update(ms, alreadySelected[6]);
+
+
+            Duck.Update(ms, alreadySelected[8]);
+
+
+
+            if (Car.IsClicked)
+            {
+                Players.Add(new Player(VroomVroom, new Vector2(1000), Color.White, "Car"));
+                alreadySelected[0] = false;
+            }
+
+            if (Boat.IsClicked)
+            {
+                Players.Add(new Player(VroomVroom, new Vector2(1000), Color.White, "Car"));
+                alreadySelected[0] = false;
+            }
+
+            if (Hat.IsClicked)
+            {
+                Players.Add(new Player(VroomVroom, new Vector2(1000), Color.White, "Car"));
+                alreadySelected[0] = false;
+            }
+
+            if (Cat.IsClicked)
+            {
+                Players.Add(new Player(VroomVroom, new Vector2(1000), Color.White, "Car"));
+                alreadySelected[0] = false;
+            }
+
+            if (Wheelbarrow.IsClicked)
+            {
+                Players.Add(new Player(VroomVroom, new Vector2(1000), Color.White, "Car"));
+                alreadySelected[0] = false;
+            }
+
+            if (Boot.IsClicked)
+            {
+                Players.Add(new Player(VroomVroom, new Vector2(1000), Color.White, "Car"));
+                alreadySelected[0] = false;
+            }
+
+            if (Car.IsClicked)
+            {
+                Players.Add(new Player(VroomVroom, new Vector2(1000), Color.White, "Car"));
+                alreadySelected[0] = false;
+            }
+
+            if (Car.IsClicked)
+            {
+                Players.Add(new Player(VroomVroom, new Vector2(1000), Color.White, "Car"));
+                alreadySelected[0] = false;
+            }
+
+            if (Duck.IsClicked)
+            {
+                Players.Add(new Player(VroomVroom, new Vector2(1000), Color.White, "Car"));
+                alreadySelected[0] = false;
+            }
+
+
         }
         public override void Draw(SpriteBatch spritebatch)
         {
