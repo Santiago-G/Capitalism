f4849d4a11e3d3b458bc9b1ce1bc575df088a235
Santiago G <Santiagoefrad@outlook.com>
2021-01-11 17:00:56 -0800

    afaefa

---
+++ Capitalism/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capitalism
+{
+    public static class Extensions
+    {
+        static Random gen = new Random();
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> enumerable)
+        {
+            var list = new List<T>(enumerable);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                int newIndex = gen.Next(i, list.Count);
+
+                // swap whatever is at i with newindex
+
+                var temp = list[i];
+                list[i] = list[newIndex];
+                list[newIndex] = temp;
+
+            }
+
+            return list;
+
+        }
+    }
+}
